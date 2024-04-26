fbb9ac5ff7ee885f28491ffecfef53dc8af2ea62
clclFL <895739647@qq.com>
2024-04-27 06:54:21 +0800

    fix(): 优化中间件启动

---
+++ frontend_py/main_cmd.py
@@ -0,0 +1,74 @@
+"""
+简易实现的命令行操作界面，提供通过命令行操作的方式来向中间件发送消息
+"""
+import cmd
+import socket
+import pickle
+
+from loguru import logger
+
+from mq_py.message import Message
+
+__version__ = '0.0.1'
+logger.info(f'Launching MainCMD program by python, current version: {__version__}')
+
+server_host = '127.0.0.1'
+server_port = 25565
+
+logger.info(f'Connecting to MessageQueue Server on {server_host}:{server_port}')
+connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection.connect((server_host, server_port))
+logger.info(f'Connect Success')
+
+
+# 接收线程
+def recv_():
+    """
+    模拟前端接收线程，负责接收消息
+    """
+    while True:
+        try:
+            data = connection.recv(4096)
+            if not data:
+                break
+            if len(data) < 4:
+                raise ValueError("Received packet is too small to contain pickle data.")
+            # 长度部分取前四个字节
+            length = int.from_bytes(data[:4:], byteorder='big')
+            if len(data) < length + 4:
+                # 如果没有接收到完整的数据，继续接收余下部分
+                # 注意这里没有设置缓冲区，需要避免接收过大体量的请求
+                remaining = length + 4 - len(data)
+                data += connection.recv(remaining)
+
+            # 解码对象，对象数据在四字节之后
+            message = pickle.loads(data[4:])
+            print(f'Received object: {message}')
+        except socket.error as e:
+            logger.debug(f'lost connection with server')
+
+
+class MainCmd(cmd.Cmd):
+    prompt = '> '
+
+    def do_send(self, line):
+        """send a Message to message queue"""
+        message = Message.from_frontend_output(line)
+
+        # 编码对象
+        data = pickle.dumps(message, protocol=pickle.HIGHEST_PROTOCOL)
+        length = len(data).to_bytes(4, byteorder='big')
+
+        # 发送数据长度
+        connection.sendall(length)
+        # 发送数据
+        connection.sendall(data)
+
+
+    def do_quit(self, line):
+        """Quit the program"""
+        return True
+
+
+if __name__ == '__main__':
+    MainCmd().cmdloop("Program already launched, Type 'help' for available commands.")
--- mq_py/main.py
+++ mq_py/main.py
@@ -6,10 +6,14 @@ Created on 2024.4.24
 """
 __version__ = '0.0.1'
 
+import cmd
+import os
 import pickle
 import queue
 import socket
 import threading
+import sys
+from collections import defaultdict
 
 import yaml
 from loguru import logger
@@ -17,10 +21,11 @@ from loguru import logger
 from message import Message
 from mq_py.properties import Properties
 
+# 配置日志输出等级
 logger.info(f'Launching MQ process by python, current version: {__version__}')
 
 # 配置文件初始化
-ROOT_DIR = '../mq_py_dir'  # 项目根路径
+ROOT_DIR = '.'  # 项目根路径
 CONFIG_PATH = f'{ROOT_DIR}/config.yaml'
 with open(CONFIG_PATH, 'r', encoding='utf-8') as f:
     yml_cfg = yaml.safe_load(f)
@@ -31,15 +36,7 @@ logger.debug(f'Loading backend process config, path: {yml_cfg["path"]["backend_c
 prop_cfg = Properties(yml_cfg['path']['backend_config']).get_prop()
 logger.debug(f'Complete loading program config')
 
-# 输入管道文件，通过输入管道文件以 [ip:port]<空格>[操作名]<空格>[请求体] 的格式写入数据
-input_pip_path = prop_cfg['input_pip_path']
-# 输入管道锁文件，完成写入之后移除锁，使后端读取消息，没有锁的时候不应该写入数据，直至后端进程归还锁
-input_pip_lock = prop_cfg['input_pip_lock']
 
-# 输出管道文件，输出管道以 [ip:port]<空格>[操作名]<空格>[返回码]<空格>[响应体]<换行符> 的格式写出数据
-output_pip_path = prop_cfg['output_pip_path']
-# 输出管道锁文件，如果不存在锁文件，可以直接读取消息，读取完成之后需要创建锁文件，存在锁文件的时候不应该读取输出管道消息
-output_pip_lock = prop_cfg['output_pip_lock']
 
 # 请求消息队列，由后端接口读取输出管道数据写入此处，由前端接口读出派发给对应前端程序
 # 格式: [ip:port]<空格>[操作名]<空格>[响应体]<换行符> 字符串
@@ -51,12 +48,70 @@ response_mq = queue.Queue()
 
 # 连接队列，通过connection_handler处理之后产生的连接对象会被存储在此处
 connection_cache = queue.Queue()
-# 连接池，通过ip和端口建立起对某个连接对象的映射，从而在消息回传的时候能够将回传消息
-# 重新映射到某个具体的前端程序上
-connection_pool = {}
+# 连接处理线程追踪器，为二维字典，以address作为键名，connection连接以及connection_thread作为键值
+connection_tracker = defaultdict(dict)
+
+# 中间件绑定ip地址
+server_host = yml_cfg['server']['host']
+# 中间件绑定端口
+server_port = yml_cfg['server']['port']
+
+# 超时时间，超时后会再次检查线程状态
+timeout = yml_cfg['server']['timeout']
+
+# 线程退出信号量
+is_terminated = False
+is_launched = False
 
 
-def backend_in_interface():
+# 输入管道文件，通过输入管道文件以 [ip:port]<空格>[操作名]<空格>[请求体] 的格式写入数据
+input_pip_path = prop_cfg['input_pip_path']
+# 输入管道锁文件，完成写入之后移除锁，使后端读取消息，没有锁的时候不应该写入数据，直至后端进程归还锁
+input_pip_lock = prop_cfg['input_pip_lock']
+
+# 输出管道文件，输出管道以 [ip:port]<空格>[操作名]<空格>[返回码]<空格>[响应体]<换行符> 的格式写出数据
+output_pip_path = prop_cfg['output_pip_path']
+# 输出管道锁文件，如果不存在锁文件，可以直接读取消息，读取完成之后需要创建锁文件，存在锁文件的时候不应该读取输出管道消息
+output_pip_lock = prop_cfg['output_pip_lock']
+
+# 检测管道文件是否存在
+if not os.path.exists(input_pip_path):
+    raise FileNotFoundError(f'Cannot find input pip file at : {input_pip_path}')
+
+if not os.path.exists(output_pip_path):
+    raise FileNotFoundError(f'Cannot find output pip file at : {output_pip_path}')
+
+
+class connection_builder(threading.Thread):
+    def run(self):
+        """
+        创建连接对象线程，连接被创建之后会被加入connection_queue以及conn_pool当中
+        """
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((server_host, server_port))
+        # 设置超时时间，避免等待时间过长
+        server.settimeout(timeout)
+        server.listen()
+        logger.info(f'Connection Builder launched, listening connection from {server_host}:{server_port}')
+
+        global is_terminated
+        while not is_terminated:
+            try:
+                connection, address = server.accept()
+                logger.info(f'Accept connection from {address[0]}:{address[1]}')
+                connection_tracker[address]['connection'] = connection
+                # 将连接对象添加进入连接池
+                connection_cache.put((connection, address))
+            except socket.timeout:
+                # 如果线程已经停止运行，那么退出程序
+                if is_terminated:
+                    break
+        # 线程退出，关闭服务器
+        server.close()
+        logger.debug('Middleware Server shutdown')
+
+
+class backend_input_middleware(threading.Thread):
     """
     消息中间件 -> 后端输入管道
     读取request_mq消息队列获取请求消息，将消息封装成为后端可读格式，写入后端输入管道中，完成写入之后
@@ -66,10 +121,24 @@ def backend_in_interface():
     消息格式-消息中间件  : Message实例
 
     """
-    ...
+
+    def run(self) -> None:
+        logger.debug('Backend-Input-Middleware interface launched')
+        global is_terminated, timeout
+        while not is_terminated:
+            try:
+                request_message: str = request_mq.get(timeout=timeout)
+
+                print(request_message)
+
+            except queue.Empty:
+                # 超时退出
+                if is_terminated:
+                    break
+        # 线程退出
 
 
-def backend_out_interface():
+class backend_output_middleware(threading.Thread):
     """
     后端输出管道 -> 消息中间件
     读取后端输出管道消息，封装成为Message对象加入response_mq当中，锁文件output_pip_lock不存在的时候
@@ -80,10 +149,13 @@ def backend_out_interface():
     消息格式-response_mq : [ip:port]<空格>[操作名]<空格>[返回码]<空格>[响应体]<换行符>
 
     """
-    ...
+
+    def run(self) -> None:
+        logger.debug('Backend-Output-Middleware interface launched')
+        ...
 
 
-def frontend_in_interface():
+class frontend_output_middleware(threading.Thread):
     """
     前端进程 -> 消息中间件
     通过connection_cache缓冲池获取前端连接实例，处理实例构建处理线程，线程创建 消息中间件 和 某一个前端进程 的长连接
@@ -92,8 +164,26 @@ def frontend_in_interface():
     消息格式-前端进程    : Message实例
 
     """
+    def run(self):
+        logger.debug('Frontend-Input-Middleware interface launched')
 
-    def handle_connection(conn, addr):
+        # 从连接缓冲区获取连接对象
+        global is_terminated
+        while not is_terminated:
+            try:
+                # 设置超时事件，避免退出后线程阻塞
+                connection, address = connection_cache.get(timeout=timeout)
+                connection.settimeout(timeout)
+                connection_thread = threading.Thread(target=self.handle_connection, args=(connection, address))
+                # 将处理线程添加进入线程池中
+                connection_tracker[address]['connection_thread'] = connection_thread
+                connection_thread.start()
+            except queue.Empty:
+                if is_terminated:
+                    break
+        logger.debug('Frontend-Input-Middleware interface shutdown')
+
+    def handle_connection(self, connection, address):
         """
         连接处理线程，主要是将address中的ip地址和port信息加入消息中，封装成为 管道消息格式 ，然后加入
         request_mq当中，等待输入进程将消息写入管道文件中
@@ -102,9 +192,10 @@ def frontend_in_interface():
         消息格式-request_mq : [ip:port]<空格>[操作名]<空格>[请求体]<换行符> （字符串形式）
 
         """
-        while True:
+        global is_terminated
+        while not is_terminated:
             try:
-                data = conn.recv(4096)
+                data = connection.recv(4096)
                 if not data:
                     break
                 if len(data) < 4:
@@ -115,22 +206,30 @@ def frontend_in_interface():
                     # 如果没有接收到完整的数据，继续接收余下部分
                     # 注意这里没有设置缓冲区，需要避免接收过大体量的请求
                     remaining = length + 4 - len(data)
-                    data += conn.recv(remaining)
+                    data += connection.recv(remaining)
 
                 # 解码对象，对象数据在四字节之后
                 message = pickle.loads(data[4:])
-                print(f'Received object: {message}')
-            except socket.error as e:
-                logger.debug(f'Connection close : {addr[0]}:{addr[1]}')
+                # 或取当前用户ip地址，为message设置
+                message.setAddress(address)
 
-    # 从连接缓冲区获取连接对象
-    while True:
-        connection, address = connection_cache.get()
-        # 创建新的线程处理连接请求
-        threading.Thread(target=handle_connection, args=(connection, address))
+                # 将消息转化为字符串格式加入队列中
+                request_mq.put(message.to_backend_input())
+
+            except socket.timeout as e:
+                if is_terminated:
+                    connection.close()
+                    logger.debug(f'Connection timeout : {address[0]}:{address[1]}')
+                    return
+
+        # 用户线程退出，关闭连接
+        connection.close()
+        # 不再追踪该地址信息
+        connection_tracker.pop(address)
+        logger.debug(f'Connection close : {address[0]}:{address[1]}')
 
 
-def frontend_out_interface():
+class frontend_input_middleware(threading.Thread):
     """
     前端响应线程，将response_mq当中已有消息取出，然后根据Message格式解析，同时获取源的ip以及端口，通过
     connection_pool获取原始连接对象，将封装好的Message返回给前端进程，前端进程通过函数接收
@@ -139,25 +238,70 @@ def frontend_out_interface():
     消息格式-response_mq : [ip:port]<空格>[操作名]<空格>[返回码]<空格>[响应体]<换行符> （字符串形式）
 
     """
-    ...
+
+    def run(self):
+        logger.debug('Frontend-Output-Middleware interface launched')
+        ...
 
 
-def connection_builder():
+class middleware_cmd(cmd.Cmd):
     """
-    创建连接对象线程，连接被创建之后会被加入connection_queue以及conn_pool当中
-    :return:
+    命令行界面构建
+    提供简单的指令，用于对消息中间件程序进行流程控制
     """
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    prompt = '> '
 
-    host = socket.gethostname()
-    port = 25565
-    server.bind((host, 25565))
-    server.listen()
-    logger.debug(f'Server launch on {host}:{port}')
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.conn_builder = connection_builder()  # 连接处理器
 
-    while True:
-        connection, address = server.accept()
-        print(f'Accept connection from client: {address}, submit to sub-thread')
-        connection_pool[address] = connection
-        # 将连接对象添加进入连接池
-        connection_cache.put((connection, address))
+        self.interface_1 = frontend_output_middleware()  # 前端 -> 中间件
+        self.interface_2 = backend_input_middleware()  # 中间件 -> 后端
+        self.interface_3 = backend_output_middleware()  # 后端 -> 中间件
+        self.interface_4 = frontend_input_middleware()  # 中间件 -> 前端
+
+        """Launch MessageQueue program"""
+        global is_launched
+        if is_launched:
+            logger.info('Middleware has already launched')
+            return
+
+        # 启动接口
+        is_launched = True
+        self.conn_builder.start()
+        self.interface_1.start()
+        self.interface_2.start()
+        self.interface_3.start()
+        self.interface_4.start()
+
+    def do_quit(self, line):
+        """ 关闭所有消息中间件所有接口之后退出程序 """
+        # 触发关闭事件
+        global is_terminated, timeout
+        is_terminated = True
+        # 为连接设置超时时间
+
+        # 等待线程全部退出
+        logger.info('Waiting middleware shutdown...')
+        self.conn_builder.join()
+        self.interface_1.join()
+        self.interface_2.join()
+        self.interface_3.join()
+        self.interface_4.join()
+
+        # 等待所有连接线程退出
+        for address, connection_bundle in connection_tracker.items():
+            connection_bundle['connection_thread'].join()
+
+        logger.info('Middleware successfully shutdown')
+        return True
+
+    # 默认指令
+    def default(self, line):
+        """Default action for any command not recognized"""
+        print("指令未识别，键入 'help' 来查询可用的指令.")
+
+
+if __name__ == '__main__':
+    # 启动命令行程序，避免进程退出
+    middleware_cmd().cmdloop("Successfully launch Middleware for DM Script, input 'help' for available command.")
